1e6577caef8c2e3904ff718ca322b5f9e6abeea6
Zenahr Barzani <zenmatica@gmail.com>
2020-04-13 21:32:24 +0200

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+if __name__ == '__main__':
+    app.run(debug=True)
