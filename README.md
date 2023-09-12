a8c183a07c39d41e83a91ddecfd8f8ab2b95fade
Maxim Sablev <maxim.sablev@gmail.com>
2023-09-12 21:03:31 +0300

    build(pipenv): зафиксировать версии библиотек build(docker): собирать docker-образ на основе python:3.11-slim build(docker-compose): обеспечить развётывание приложения и инфраструктуры наблюдаемости doc(readme): описать настройку pipenv и развёртывание приложения feat(prometheus): выставить эндпоинт с метриками Prometheus

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # flask-sqlite3-todo-crud with blackjack and prometheus
 
+```console
+docker compose -p otus-sre-hw15-p1 up
+```
+
+## Pipenv
+
+```console
+python -m pipenv install --python 3.11
+python -m pipenv install flask==2.3.3 flask-sqlalchemy==3.1.1 gunicorn==21.2.0 prometheus-flask-exporter==0.22.4
+```
+
 ## Dockerize
 
 Build container:
--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask import render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path, PurePath
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{PurePath.joinpath(Path(__file__).resolve().parent, 'todo.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.0')
 
 db = SQLAlchemy(app)
 
@@ -32,11 +36,12 @@ def add():
 @app.route('/update', methods=["POST"])
 def update():
     completed_ids = [int(i) for i in request.form]
-    for todo in Todo.query.all():
+    todos = Todo.query.all()
+    for todo in todos:
         setattr(todo, 'complete', False if todo.id not in completed_ids else True)
     db.session.commit()
     # return request.form # DEBUG REQUEST
     return redirect(url_for("index"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
